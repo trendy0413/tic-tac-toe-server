9e9e4821afdc7168d85a137cfd2cbf8ba84e3c80
trendy0413 <trendy0413dev@gmail.com>
2022-04-11 21:45:09 +0530

    cors updated

--- src/server.ts
+++ src/server.ts
@@ -28,11 +28,12 @@ var server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 
+const io = socketServer(server);
+
 server.listen(port);
 server.on("error", onError);
 server.on("listening", onListening);
 
-const io = socketServer(server);
 
 /**
  * Normalize a port into a number, string, or false.
